21b90bafb5c7154119e579496141bf20131b87c1
ertoth <ertoth@paypal.com>
2014-01-15 21:44:38 -0800

    initial

---
+++ README.md
@@ -0,0 +1,11 @@
+```javascript
+'use strict';
+
+var express = require('express'),
+    kraken = require('../');
+
+
+var app = express();
+app.use(kraken());
+app.listen(8000);
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var path = require('path'),
+    caller = require('caller'),
+    express = require('express'),
+    bootstrap = require('./lib/bootstrap');
+
+
+module.exports = function (options) {
+    var app;
+
+    if (typeof options === 'string') {
+        options = { basedir: options };
+    }
+
+    options = options || {};
+    options.basedir = options.basedir || path.dirname(caller());
+
+    app = express();
+    app.once('mount', function (parent) {
+        var startup;
+
+        // Remove sacrificial express app
+        parent.stack.pop();
+
+        // Kick off server and add middleware which will block
+        // until server is ready. This way we don't have to block
+        // standard `listen` behavior.
+        startup = bootstrap(parent, options);
+        parent.use(function (req, res, next) {
+            startup.done(next);
+        });
+    });
+    return app;
+};
