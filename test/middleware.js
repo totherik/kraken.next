ce9185945f86b057fd354ac8986d6286752fcc8a
ertoth <ertoth@paypal.com>
2014-01-20 15:27:02 -0800

    middleware coverage

---
+++ test/middleware.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var test = require('tape'),
+    path = require('path'),
+    kraken = require('../'),
+    nconf = require('nconf'),
+    express = require('express');
+
+test('middleware', function (t) {
+
+    function reset() {
+        nconf.stores  = {};
+        nconf.sources = [];
+    }
+
+    t.test('middleware', function (t) {
+        t.end();
+    });
+
+
+    t.test('no config', function (t) {
+        var basedir, app;
+
+        t.on('end', reset);
+
+        basedir = path.join(__dirname, 'fixtures', 'middleware');
+        app = express();
+        app.on('start', t.end.bind(t));
+        app.on('error', t.error.bind(t));
+        app.use(kraken(basedir));
+    });
+
+});
